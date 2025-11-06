e44feeb1f5d4e641adb35d99d8a07e591797c352
NaviGamage <navinduprabashwara@gmail.com.com>
2025-11-06 15:22:58 +0530

    implement AdminDashboardPageController and Add AdminDashboard.png image

--- src/main/java/controller/AdminDashboardPageController.java
+++ src/main/java/controller/AdminDashboardPageController.java
@@ -1,4 +1,56 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 public class AdminDashboardPageController {
+
+    @FXML
+    private Button btnBook;
+
+    @FXML
+    private Button btnCustomer;
+
+    @FXML
+    private Button btnDashboard;
+
+    @FXML
+    private Button btnRentalBooks;
+
+    @FXML
+    private Button btnRetunrBooks;
+
+    @FXML
+    private Label lblToatalBooks;
+
+    @FXML
+    private Label lblToatalCustomer;
+
+    @FXML
+    void btnBookOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnCustomerOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDashboardOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnRentalBooksOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnRetunrBooksOnAction(ActionEvent event) {
+
+    }
+
 }
