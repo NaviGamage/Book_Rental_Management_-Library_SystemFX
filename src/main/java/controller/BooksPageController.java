1f62920bc419c6f285acffb4934654c7b3fee502
NaviGamage <navinduprabashwara@gmail.com.com>
2025-11-06 23:03:39 +0530

    Implement BooksPageController

--- src/main/java/controller/BooksPageController.java
+++ src/main/java/controller/BooksPageController.java
@@ -1,4 +1,120 @@
 package controller;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+
 public class BooksPageController {
+
+    @FXML
+    private JFXButton btnAddBook;
+
+    @FXML
+    private Button btnBook;
+
+    @FXML
+    private Button btnClearBook;
+
+    @FXML
+    private Button btnCustomer;
+
+    @FXML
+    private Button btnDashboard;
+
+    @FXML
+    private JFXButton btnDeleteBook;
+
+    @FXML
+    private Button btnRentalBooks;
+
+    @FXML
+    private Button btnRetunrBooks;
+
+    @FXML
+    private JFXButton btnUpdateBook;
+
+    @FXML
+    private TableColumn<?, ?> colAuthor;
+
+    @FXML
+    private TableColumn<?, ?> colCategory;
+
+    @FXML
+    private TableColumn<?, ?> colID;
+
+    @FXML
+    private TableColumn<?, ?> colISBM;
+
+    @FXML
+    private TableColumn<?, ?> colName;
+
+    @FXML
+    private TableColumn<?, ?> colQuntity;
+
+    @FXML
+    private JFXTextField txtAuthor;
+
+    @FXML
+    private JFXTextField txtBookID;
+
+    @FXML
+    private JFXTextField txtBookName;
+
+    @FXML
+    private JFXTextField txtCategory;
+
+    @FXML
+    private JFXTextField txtISBM;
+
+    @FXML
+    private JFXTextField txtQuntity;
+
+    @FXML
+    void btnAddBookOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnBookOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnClearBookOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnCustomerOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDashboardOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnDeleteBookOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnRentalBooksOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnRetunrBooksOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnUpdateBookOnAction(ActionEvent event) {
+
+    }
+
 }
