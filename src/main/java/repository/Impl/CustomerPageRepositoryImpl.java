23977a9f53175def81d31d23f36050a60e7f1ab0
NaviGamage <navinduprabashwara@gmail.com.com>
2025-11-08 13:16:32 +0530

    Update the code CustomerPageRepositoryImpl

--- src/main/java/repository/Impl/CustomerPageRepositoryImpl.java
+++ src/main/java/repository/Impl/CustomerPageRepositoryImpl.java
@@ -19,15 +19,26 @@ public class CustomerPageRepositoryImpl implements CustomerPageRepository {
     }
 
     @Override
-    public void addCustomer(String custID, String name, String phone, String email, String address) {
+    public ResultSet getSearchedCustomers(String Cust_ID) throws SQLException {
+        String SQL = "SELECT * FROM customer WHERE Cust_ID = ?";
+        Connection connection = DBConnection.getInstance().getConnection();
+        PreparedStatement pstm = connection.prepareStatement(SQL);
+        pstm.setObject(1,Cust_ID);
+        ResultSet resultSet = pstm.executeQuery();
+        return resultSet;
+
+    }
+
+    @Override
+    public void addCustomer(String Cust_ID, String Name, String Phone, String Email, String Address) {
         try {
             Connection connection = DBConnection.getInstance().getConnection();
-            PreparedStatement pstm = connection.prepareStatement("INSERT INTO customer(String custID, String name, String phone, String email, String address) VALUES(?,?,?,?,?)");
-            pstm.setObject(1,custID);
-            pstm.setObject(2,name);
-            pstm.setObject(3,phone);
-            pstm.setObject(4,email);
-            pstm.setObject(5,address);
+            PreparedStatement pstm = connection.prepareStatement("INSERT INTO customer(Cust_ID,  Name,  Phone,  Email,  Address) VALUES(?,?,?,?,?)");
+            pstm.setObject(1,Cust_ID);
+            pstm.setObject(2,Name);
+            pstm.setObject(3,Phone);
+            pstm.setObject(4,Email);
+            pstm.setObject(5,Address);
             pstm.executeUpdate();
 
         } catch (SQLException e) {
@@ -36,12 +47,13 @@ public class CustomerPageRepositoryImpl implements CustomerPageRepository {
 
     }
 
+
     @Override
-    public void deleteCustomer(String custID) {
+    public void deleteCustomer(String Cust_ID) {
         try {
             Connection connection = DBConnection.getInstance().getConnection();
             PreparedStatement pstm = connection.prepareStatement("DELETE FROM customer WHERE Cust_ID = ?");
-            pstm.setObject(1,custID);
+            pstm.setObject(1,Cust_ID);
             pstm.executeUpdate();
         } catch (SQLException e) {
             throw new RuntimeException(e);
@@ -50,16 +62,17 @@ public class CustomerPageRepositoryImpl implements CustomerPageRepository {
     }
 
     @Override
-    public void updateCustomer(String custID, String name, String phone, String email, String address) {
+    public void updateCustomer(String Cust_ID, String Name, String Phone, String Email, String Address) {
 
         try {
             Connection connection = DBConnection.getInstance().getConnection();
             PreparedStatement pstm =connection.prepareStatement("UPDATE customer SET Cust_ID=?, Name=?, Phone=?, Email=?, Address=? WHERE Cust_ID=?");
-            pstm.setObject(1,custID);
-            pstm.setObject(2,name);
-            pstm.setObject(3,phone);
-            pstm.setObject(4,email);
-            pstm.setObject(5,address);
+            pstm.setObject(1, Cust_ID);
+            pstm.setObject(2, Name);
+            pstm.setObject(3, Phone);
+            pstm.setObject(4, Email);
+            pstm.setObject(5, Address);
+            pstm.setObject(6,Cust_ID);
             pstm.executeUpdate();
         } catch (SQLException e) {
             throw new RuntimeException(e);
